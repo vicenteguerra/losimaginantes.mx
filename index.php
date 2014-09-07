f5d27d98c5e5bdc3306bb687ff53b426dee3b5cd
Azelegna <Azelegna@MacBook-Air-de-Aida.local>
2014-09-07 18:16:00 -0500

    Widget Facebook
    Duplicación de variables en W de FB

--- index.php
+++ index.php
@@ -70,20 +70,18 @@
   
   		 
 
-
-  		 <section id="Widgets"><!--  ********  INICIA WIDGETS ******** -->
+<section id="Widgets"><!--  ********  INICIA WIDGETS ******** -->
   
-
-  		  	<div id="cbp-fwslider" class="cbp-fwslider">
-				<ul>
-					<li>
-						<section id="FacebookWidget">
-     					 	<iframe id="WidgetFacebook" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FImaginantesMX&amp;width=330&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; " allowTransparency="true"></iframe>
+<div id="cbp-fwslider" class="cbp-fwslider">
+<ul>
+<li>
+<section id="FacebookWidget">
+<iframe id="FacebookWidget" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FImaginantesMX&amp;width=330&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; " allowTransparency="true"></iframe>
 						 </section>
 					</li>
 					<li>
 
-						<section id="TwitterWidget">
+<section id="TwitterWidget">
 					      <a class="twitter-timeline" href="https://twitter.com/losimaginantes" data-widget-id="423161448521408512">Tweets por @losimaginantes</a>
 					      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
       					</section>
@@ -91,7 +89,7 @@
 					</li>
 					<li>
 
-						  <section id="InstagramWidget">
+<section id="InstagramWidget">
 					      		<script src="http://instansive.com/widget/js/instansive.js"></script><iframe src="http://instansive.com/widgets/ic73a113cf256862bf940a9a5b57c3a2.html" scrolling="no" allowtransparency="true" class="instansive-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
 					    </section>
 
