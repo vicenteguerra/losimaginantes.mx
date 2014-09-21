22d92e473a35fc550aff06c9f5c906818175e34d
amppumas <amp_pumas@hotmail.com>
2014-09-21 15:43:26 -0500

    Mas leido
    Solo falta la barra rosa sin hover, pero si la pongo automaticamente la
    convierte a blanco y negro....

--- functions.php
+++ functions.php
@@ -61,5 +61,17 @@ function linkCategory($categoria){
     $category_link = get_category_link( $category_id );
     return $category_link;
 }
-
+function wpb_vistas_por_posts($postID) {
+    $count_key = 'wpb_vistas_por_posts';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 ?>
--- single.php
+++ single.php
@@ -1,5 +1,9 @@
-<?php get_header();?>
-<?php get_sidebar(); ?>
+<?php 
+get_header();
+get_sidebar();
+wpb_vistas_por_posts(get_the_ID());
+?>
+
 <script>
       window.fbAsyncInit = function() {
         FB.init({
@@ -45,5 +49,28 @@
             <?php endif; ?>
         </section>
     </section>
+</section>
+<section id="postRelated">
+    <?php  
+    $mes = date( 'm' );
+    $año = date( 'Y' );
+    $my_query =new wp_query( array('posts_per_page'=>4,'meta_key'=>'wpb_vistas_por_posts','orderby'=>'meta_value_num','order'=>'DESC','year='.$año.'&monthnum='.$mes));
+    ?>
+    <ul id="tableThumbs">
+    <h2 id="plusleido">LO MÁS LEÍDO</h2>
+    <?php
+    while( $my_query->have_posts() ) {  
+    $my_query->the_post();  
+    ?>
+    <a class="linkrelPost" rel="external" href="<?php echo the_permalink()?>">
+        <li class="relatedThumbs" style="background-image: url(<?php echo get_first_image()?>)">
+            <p class="titlePost"><?php echo the_title(); ?></p>
+        </li>
+    </a>
+    <?php }  
+    $post = $orig_post;  
+    wp_reset_query();  
+    ?>
+    </ul>
 </section>
 <?php get_footer(); ?>
