83366360b9e968455e4ff072748946ee30ac8b20
Vicente Guerra Hernández <vicenteguerrahdez@gmail.com>
2015-04-26 17:21:42 -0500

    Favicon & new banners
    Marvin Festival & Ceremonia 15

--- ads.php
+++ ads.php
@@ -1,8 +1,8 @@
 <section id="contenedorPublicidad">
         <section id="publicidadL">
-           	<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ViveLatino.png">
+           	<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ceremonia15.gif">
         </section>
          <section id="publicidadR">
-            <a href="http://www.losimaginantes.mx/vive-latino/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ViveLatino.jpg"></a>
+            <a href="http://www.losimaginantes.mx/vive-latino/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/marvin15.png"></a>
         </section>
 </section>
--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
+
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <title>
 	<?php if ( is_home() ) { ?><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?><?php } ?>
@@ -16,6 +17,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" type="image/png" />
 
 
 
